dd41826c1921b71a9b4d41636cbee39c69c98964
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2025-06-30 17:00:36 +0300

    feat: add role (CRUD)

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -2,16 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\RoleResource\Pages;
-use App\Filament\Resources\RoleResource\RelationManagers;
-use App\Models\Role;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Spatie\Permission\Models\Role;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\RoleResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\RoleResource\RelationManagers;
 
 class RoleResource extends Resource
 {
@@ -19,11 +22,31 @@ class RoleResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-user-plus';
 
+    public static function getLabel(): string
+    {
+        return __('keywords.role');
+    }
+
+    public static function getPluralLabel(): string
+    {
+        return __('keywords.roles');
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->label(__('keywords.name'))
+                    ->maxLength(255),
+                Select::make('permissions')
+                    ->relationship('permissions', 'name')
+                    ->multiple()
+                    ->searchable()
+                    ->preload()
+                    ->label(__('keywords.permissions'))
+                    ->required(),
             ]);
     }
 
@@ -31,7 +54,8 @@ class RoleResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->searchable()->sortable()->searchable()->label(__('keywords.name')),
             ])
             ->filters([
                 //
